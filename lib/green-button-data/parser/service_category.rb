b15e2848b39a15335c4e72c25570a8627372a929
Andrew Jo <andrewjo@gmail.com>
2015-07-30 00:26:52 -0700

    Add namespaced elements

--- lib/green-button-data/parser/service_category.rb
+++ lib/green-button-data/parser/service_category.rb
@@ -6,6 +6,13 @@ module GreenButtonData
       element :kind, class: Integer do |kind|
         SERVICE_CATEGORY[kind]
       end
+
+      # ESPI Namespacing
+      element :'espi:kind', class: Integer, as: :kind
+
+      # Special case for PG&E which uses generic namespacing
+      element :'ns0:kind', class: Integer, as: :kind
+      element :'ns1:kind', class: Integer, as: :kind
     end
   end
 end
