5a0a475e6e39b43baeebb1575ba4ae3ec7b04543
Andrew Jo <andrewjo@gmail.com>
2015-07-29 18:01:26 -0700

    Period parser

--- lib/green-button-data/parser/period.rb
+++ lib/green-button-data/parser/period.rb
@@ -4,8 +4,23 @@ module GreenButtonData
       include SAXMachine
       include Utilities
 
-      element :duration, class: Integer { |t| normalize_epoch t }
-      element :start, class: Integer { |t| normalize_epoch t }
+      element :duration, class: Integer do |t|
+        normalize_epoch t
+      end
+
+      element :start, class: Integer do |t|
+        normalize_epoch t
+      end
+
+      # Standard ESPI namespacing
+      element :'espi:duration', class: Integer, as: :duration
+      element :'espi:start', class: Integer, as: :start
+
+      # Special case for PG&E which uses generic namespacing
+      element :'ns0:duration', class: Integer, as: :duration
+      element :'ns0:start', class: Integer, as: :start
+      element :'ns1:duration', class: Integer, as: :duration
+      element :'ns1:start', class: Integer, as: :start
     end
   end
 end
