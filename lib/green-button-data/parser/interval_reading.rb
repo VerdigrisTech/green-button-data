166be45ce6960302a18fb5180b7aa718093ff6ea
Andrew Jo <andrewjo@gmail.com>
2015-07-30 12:29:26 -0700

    Add GreenButton extension specification

--- lib/green-button-data/parser/interval_reading.rb
+++ lib/green-button-data/parser/interval_reading.rb
@@ -6,19 +6,31 @@ module GreenButtonData
       element :cost, class: Integer
       element :timePeriod, class: Interval, as: :time_period
       element :value, class: Integer
+      element :consumptionTier, class: Integer, as: :consumption_tier
+      element :tou, class: Integer
+      element :cpp, class: Integer
 
       # Standard ESPI namespacing
       element :'espi:cost', class: Integer, as: :cost
       element :'espi:timePeriod', class: Interval, as: :time_period
       element :'espi:value', class: Integer, as: :value
+      element :'espi:consumptionTier', class: Integer, as: :consumption_tier
+      element :'espi:tou', class: Integer, as: :tou
+      element :'espi:cpp', class: Integer, as: :cpp
 
       # Special case for PG&E which uses generic namespacing
       element :'ns0:cost', class: Integer, as: :cost
       element :'ns0:timePeriod', class: Interval, as: :time_period
       element :'ns0:value', class: Integer, as: :value
+      element :'ns0:consumptionTier', class: Integer, as: :consumption_tier
+      element :'ns0:tou', class: Integer, as: :tou
+      element :'ns0:cpp', class: Integer, as: :cpp
       element :'ns1:cost', class: Integer, as: :cost
       element :'ns1:timePeriod', class: Interval, as: :time_period
       element :'ns1:value', class: Integer, as: :value
+      element :'ns1:consumptionTier', class: Integer, as: :consumption_tier
+      element :'ns1:tou', class: Integer, as: :tou
+      element :'ns1:cpp', class: Integer, as: :cpp
     end
   end
 end
