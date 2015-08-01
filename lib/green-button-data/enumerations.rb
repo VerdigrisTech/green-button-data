ace357275e659bbf8e25e089283655336a6455c5
Andrew Jo <andrewjo@gmail.com>
2015-07-31 21:42:18 -0700

    Add quality of reading

--- lib/green-button-data/enumerations.rb
+++ lib/green-button-data/enumerations.rb
@@ -240,6 +240,23 @@ module GreenButtonData
       155 => :volumetric_flow
     }
 
+    QUALITY_OF_READING = {
+       0 => :valid,
+       7 => :manually_edited,
+       8 => :estimated_using_reference_day,
+       9 => :estimated_using_linear_interpolation,
+      10 => :questionable,
+      11 => :derived,
+      12 => :projected_forecast,
+      13 => :mixed,
+      14 => :raw,
+      15 => :normalized_for_weather,
+      16 => :other,
+      17 => :validated,
+      18 => :verified,
+      19 => :revenue_quality
+    }
+
     PHASE_CODE = {
       0 => :none,
       16 => :n,
