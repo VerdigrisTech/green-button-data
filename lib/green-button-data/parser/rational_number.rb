c8bd4989146b310c29a551ca30417d2a3e702c24
Andrew Jo <andrewjo@gmail.com>
2015-08-01 01:25:14 -0700

    Add alias attribute names

--- lib/green-button-data/parser/rational_number.rb
+++ lib/green-button-data/parser/rational_number.rb
@@ -11,12 +11,12 @@ module GreenButtonData
       end
 
       # ESPI Namespacing
-      element :'espi:numerator', class: Integer
-      element :'espi:denominator', class: Integer
+      element :'espi:numerator', class: Integer, as: :numerator
+      element :'espi:denominator', class: Integer, as: :denominator
 
       # Special case for PG&E which uses generic namespacing
-      element :'ns0:numerator', class: Integer
-      element :'ns0:denominator', class: Integer
+      element :'ns0:numerator', class: Integer, as: :numerator
+      element :'ns0:denominator', class: Integer, as: :denominator
     end
   end
 end
