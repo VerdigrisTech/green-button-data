42c31f75e80675381d592681084a3bf7f900f347
Andrew Jo <andrewjo@gmail.com>
2015-08-01 12:30:34 -0700

    Add framework around fixtures

---
+++ spec/fixtures.rb
@@ -0,0 +1,17 @@
+module Fixtures
+  FIXTURES = {
+    espi_application_information: "ESPIApplicationInformation.xml"
+  }
+
+  ##
+  # Dynamically load fixtures and define them as methods
+  FIXTURES.each do |method, filename|
+    define_method(method) { load_fixture filename }
+  end
+
+  ##
+  # Reads in the XML fixture file
+  def load_fixture(filename)
+    File.read "#{File.dirname __FILE__}/fixtures/#{filename}"
+  end
+end
