a1e76c5b3e7774c00ab2352b9d334011b8c9faac
Andrew Jo <andrewjo@gmail.com>
2015-08-01 14:00:52 -0700

    Run code coverage during tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,12 @@
 require File.expand_path(File.dirname(__FILE__) + '/../lib/green-button-data')
 require 'fixtures'
+require 'simplecov'
+
+SimpleCov.start
+if ENV['CI'] == 'true'
+  require 'codecov'
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
 
 SAXMachine.handler = ENV['HANDLER'].to_sym if ENV['HANDLER']
 
