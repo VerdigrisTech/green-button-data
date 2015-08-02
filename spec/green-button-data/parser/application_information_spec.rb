694f1ca0f5a11e23bc599dcfe3b5a9bc9d972a72
Andrew Jo <andrewjo@gmail.com>
2015-08-01 18:24:56 -0700

    ApplicationInformation spec

---
+++ spec/green-button-data/parser/application_information_spec.rb
@@ -0,0 +1,153 @@
+require "#{File.dirname(__FILE__)}/../../spec_helper"
+
+describe GreenButtonData::Parser::ApplicationInformation do
+  context "espi namespace" do
+    let(:feed) { GreenButtonData::Parser::Feed }
+    let :application_information do
+      feed.parse(espi_application_information)
+          .entries
+          .content
+          .application_information
+    end
+
+    subject { application_information }
+
+    it "should parse data custodian id" do
+      expect(subject.data_custodian_id).to eq "data_custodian"
+    end
+
+    it "should parse data custodian application status" do
+      expect(subject.data_custodian_application_status).to eq 1
+    end
+
+    it "should parse data custodian scope selection URI" do
+      expect(subject.data_custodian_scope_selection_screen_uri).to eq "https://services.greenbuttondata.org//DataCustodian/RetailCustomer/ScopeSelectionList?ThirdPartyID=third_party"
+    end
+
+    it "should parse data custodian bulk request URI" do
+      expect(subject.data_custodian_bulk_request_uri).to eq "https://services.greenbuttondata.org//DataCustodian/espi/1_1/resource/Batch/Bulk"
+    end
+
+    it "should parse data custodian resource endpoint" do
+      expect(subject.data_custodian_resource_endpoint).to eq "https://services.greenbuttondata.org//DataCustodian/espi/1_1/resource"
+    end
+
+    it "should parse third party application type" do
+      expect(subject.third_party_application_type).to eq 1
+    end
+
+    it "should parse third party application use" do
+      expect(subject.third_party_application_use).to eq 1
+    end
+
+    it "should parse third party application description" do
+      expect(subject.third_party_application_description).to eq "EnergyOS OpenESPI Example Third Party"
+    end
+
+    it "should parse third party scope selection URI" do
+      expect(subject.third_party_scope_selection_screen_uri).to eq "https://services.greenbuttondata.org/ThirdParty/RetailCustomer/ScopeSelection"
+    end
+
+    it "should parse third party user portal URI" do
+      expect(subject.third_party_user_portal_screen_uri).to eq "https://services.greenbuttondata.org/ThirdParty"
+    end
+
+    it "should parse third party notify URI" do
+      expect(subject.third_party_notify_uri).to eq "https://services.greenbuttondata.org/ThirdParty/espi/1_1/Notification"
+    end
+
+    it "should parse authorization server URI" do
+      expect(subject.authorization_server_uri).to eq "https://services.greenbuttondata.org//DataCustodian/"
+    end
+
+    it "should parse authorization server's authorization endpoint" do
+      expect(subject.authorization_server_authorization_endpoint).to eq "https://services.greenbuttondata.org//DataCustodian/oauth/authorize"
+    end
+
+    it "should parse authorization server's registration endpoint" do
+      expect(subject.authorization_server_registration_endpoint).to eq "https://services.greenbuttondata.org//DataCustodian/espi/1_1/register"
+    end
+
+    it "should parse authorization server's token endpoint" do
+      expect(subject.authorization_server_token_endpoint).to eq "https://services.greenbuttondata.org//DataCustodian/oauth/token"
+    end
+
+    it "should parse token endpoint's authentication method" do
+      expect(subject.token_endpoint_auth_method).to eq "client_secret_basic"
+    end
+
+    it "should parse client name" do
+      expect(subject.client_name).to eq "Green Button Third Party"
+    end
+
+    it "should parse client id" do
+      expect(subject.client_id).to eq "third_party"
+    end
+
+    it "should parse client secret" do
+      expect(subject.client_secret).to eq "secret"
+    end
+
+    it "should parse date when client ID was issued" do
+      expect(subject.client_id_issued_at).to eq DateTime.new 2014, 6, 19, 15
+    end
+
+    it "should parse when client secret expires" do
+      expect(subject.client_secret_expires_at).to eq DateTime.new 9999, 12, 31, 23, 59, 59
+    end
+
+    it "should parse redirect URI" do
+      expect(subject.redirect_uri).to eq "https://services.greenbuttondata.org/ThirdParty/espi/1_1/OAuthCallBack"
+    end
+
+    it "should parse software id" do
+      expect(subject.software_id).to eq "EnergyOS OpenESPI Example Third Party"
+    end
+
+    it "should parse software version" do
+      expect(subject.software_version).to eq "1.2"
+    end
+
+    it "should parse scopes" do
+      expect(subject.scopes.size).to eq 3
+      expect(subject.scopes.first).to eq "FB=1_3_4_5_13_14_39;IntervalDuration=3600;BlockDuration=monthly;HistoryLength=13"
+      expect(subject.scopes.last).to eq "FB=1_3_4_5_6_7_8_9_10_11_29_12_13_14_15_16_17_18_19_27_28_32_33_34_35_37_38_39_40_41_44;IntervalDuration=3600;BlockDuration=monthly;HistoryLength=13"
+    end
+
+    it "should parse grant types" do
+      expect(subject.grant_types.size).to eq 3
+      expect(subject.grant_types.first).to eq "authorization_code"
+    end
+
+    it "should parse response types" do
+      expect(subject.response_types.size).to eq 1
+      expect(subject.response_types.first).to eq "token"
+    end
+
+    it "should parse registration_client_uri" do
+      expect(subject.registration_client_uri).to eq "https://services.greenbuttondata.org//DataCustodian/espi/1_1/resource/ApplicationInformation/2"
+    end
+
+    it "should parse registration_access_token" do
+      expect(subject.registration_access_token).to eq "d89bb056-0f02-4d47-9fd2-ec6a19ba8d0c"
+    end
+
+    describe "#application_status" do
+      it "should return application status as a symbol" do
+        expect(subject.application_status).to eq :review
+      end
+    end
+
+    describe "#application_type" do
+      it "should return application type as a symbol" do
+        expect(subject.application_type).to eq :web
+      end
+    end
+
+    describe "#application_use" do
+      it "should return application use as a symbol" do
+        expect(subject.application_use).to eq :energy_management
+      end
+    end
+  end
+end
