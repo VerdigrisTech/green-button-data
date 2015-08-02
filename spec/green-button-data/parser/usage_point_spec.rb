036cf045628ecd855b1f8f68384288d5a44b392e
Andrew Jo <andrewjo@gmail.com>
2015-08-01 19:47:24 -0700

    Add UsagePoint spec

---
+++ spec/green-button-data/parser/usage_point_spec.rb
@@ -0,0 +1,22 @@
+require "spec_helper"
+
+describe GreenButtonData::Parser::UsagePoint do
+  context "espi namespace" do
+    let(:feed) { GreenButtonData::Parser::Feed }
+    let :usage_point do
+      feed.parse(espi_usage_point).entries.first.content.usage_point
+    end
+
+    subject { usage_point }
+
+    it "should parse kind" do
+      expect(subject.kind).to eq 0
+    end
+
+    describe "#service_category" do
+      it "should return service category as a symbol" do
+        expect(subject.service_category).to eq :electricity
+      end
+    end
+  end
+end
