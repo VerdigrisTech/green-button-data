0c385c190b43f18e9cc189e7e6b0e1eb26ae8310
Andrew Jo <andrewjo@gmail.com>
2015-08-01 19:39:28 -0700

    Add UsagePoint fixture

--- spec/fixtures.rb
+++ spec/fixtures.rb
@@ -1,7 +1,8 @@
 module Fixtures
   FIXTURES = {
     espi_application_information: "ESPIApplicationInformation.xml",
-    espi_authorization: "ESPIAuthorization.xml"
+    espi_authorization: "ESPIAuthorization.xml",
+    espi_usage_point: "ESPIUsagePoint.xml"
   }
 
   ##
