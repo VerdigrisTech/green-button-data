6f8fc6a3cb66a7968394d37150d67caadb4a3c93
Andrew Jo <andrewjo@gmail.com>
2015-08-04 08:48:34 -0700

    Add PG&E ReadingType fixture

--- spec/fixtures.rb
+++ spec/fixtures.rb
@@ -9,7 +9,8 @@ module Fixtures
     pge_application_information: "PGEApplicationInformation.xml",
     pge_authorization: "PGEAuthorization.xml",
     pge_interval_block: "PGEIntervalBlock.xml",
-    pge_local_time_parameters: "PGELocalTimeParameters.xml"
+    pge_local_time_parameters: "PGELocalTimeParameters.xml",
+    pge_reading_type: "PGEReadingType.xml"
   }
 
   ##
