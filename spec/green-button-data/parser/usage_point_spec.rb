c906a6b96e3c6c6f4e79aac3cff28f5189636359
Andrew Jo <andrewjo@gmail.com>
2015-08-04 09:04:57 -0700

    Add spec for PG&E namespace

--- spec/green-button-data/parser/usage_point_spec.rb
+++ spec/green-button-data/parser/usage_point_spec.rb
@@ -19,4 +19,23 @@ describe GreenButtonData::Parser::UsagePoint do
       end
     end
   end
+
+  context "PG&E namespace" do
+    let(:feed) { GreenButtonData::Feed }
+    let :usage_point do
+      feed.parse(espi_usage_point).entries.first.content.usage_point
+    end
+
+    subject { usage_point }
+
+    it "should parse kind" do
+      expect(subject.kind).to eq 0
+    end
+
+    describe "#service_category" do
+      it "should return service category as a symbol" do
+        expect(subject.service_category).to eq :electricity
+      end
+    end
+  end
 end
