9678c58b7c6b2d5ba1ba24c78df45636bc4350d6
Andrew Jo <andrewjo@gmail.com>
2015-08-03 21:40:12 -0700

    Add PG&E ApplicationInformation spec

--- spec/green-button-data/parser/application_information_spec.rb
+++ spec/green-button-data/parser/application_information_spec.rb
@@ -151,4 +151,158 @@ describe GreenButtonData::Parser::ApplicationInformation do
       end
     end
   end
+
+  context "PG&E namespace" do
+    let(:feed) { GreenButtonData::Feed }
+    let :application_information do
+      feed.parse(pge_application_information)
+          .entries
+          .first
+          .content
+          .application_information
+    end
+
+    subject { application_information }
+
+    it "should parse data custodian id" do
+      expect(subject.data_custodian_id).to eq "PGE"
+    end
+
+    it "should parse data custodian application status" do
+      expect(subject.data_custodian_application_status).to eq 2
+    end
+
+    it "should parse data custodian scope selection URI" do
+      expect(subject.data_custodian_scope_selection_screen_uri).to eq "https://sharemydata.pge.com/myAuthorization/?clientId=0&verified=true"
+    end
+
+    it "should parse data custodian bulk request URI" do
+      expect(subject.data_custodian_bulk_request_uri).to eq "https://api.pge.com/GreenButtonConnect/espi/1_1/resource/Batch/Bulk/0"
+    end
+
+    it "should parse data custodian resource endpoint" do
+      expect(subject.data_custodian_resource_endpoint).to eq "https://api.pge.com/GreenButtonConnect/espi/1_1/resource"
+    end
+
+    it "should parse third party application type" do
+      expect(subject.third_party_application_type).to eq 1
+    end
+
+    it "should parse third party application use" do
+      expect(subject.third_party_application_use).to eq nil
+    end
+
+    it "should parse third party application description" do
+      expect(subject.third_party_application_description).to eq "Verdigris is an energy intelligence solution that shows facilities managers how their buildings use energy at the equipment level--all in real time."
+    end
+
+    it "should parse third party phone number" do
+      expect(subject.third_party_phone).to eq "6502273888"
+    end
+
+    it "should parse third party scope selection URI" do
+      expect(subject.third_party_scope_selection_screen_uri).to eq "http://api.example.com/espi/pge"
+    end
+
+    it "should parse third party user portal URI" do
+      expect(subject.third_party_user_portal_screen_uri).to eq "https://www.example.com"
+    end
+
+    it "should parse third party notify URI" do
+      expect(subject.third_party_notify_uri).to eq "https://api.example.com/espi/pge/notify"
+    end
+
+    it "should parse authorization server URI" do
+      expect(subject.authorization_server_uri).to eq "https://api.pge.com"
+    end
+
+    it "should parse authorization server's authorization endpoint" do
+      expect(subject.authorization_server_authorization_endpoint).to eq "https://api.pge.com/datacustodian/oauth/v2/authorize"
+    end
+
+    it "should parse authorization server's registration endpoint" do
+      expect(subject.authorization_server_registration_endpoint).to eq nil
+    end
+
+    it "should parse authorization server's token endpoint" do
+      expect(subject.authorization_server_token_endpoint).to eq "https://api.pge.com/datacustodian/oauth/v2/token"
+    end
+
+    it "should parse token endpoint's authentication method" do
+      expect(subject.token_endpoint_auth_method).to eq "client_secret_basic"
+    end
+
+    it "should parse client name" do
+      expect(subject.client_name).to eq "Verdigris Technologies, Inc."
+    end
+
+    it "should parse client id" do
+      expect(subject.client_id).to eq "verdigris"
+    end
+
+    it "should parse client secret" do
+      expect(subject.client_secret).to eq "foobar"
+    end
+
+    it "should parse date when client ID was issued" do
+      expect(subject.client_id_issued_at).to eq DateTime.new 2015, 10, 21, 16, 21, 21
+    end
+
+    it "should parse when client secret expires" do
+      expect(subject.client_secret_expires_at).to eq DateTime.new 9999, 12, 31, 23, 59, 59
+    end
+
+    it "should parse redirect URI" do
+      expect(subject.redirect_uri).to eq "http://api.example.com/espi/pge/callback"
+    end
+
+    it "should parse software id" do
+      expect(subject.software_id).to eq "NA"
+    end
+
+    it "should parse software version" do
+      expect(subject.software_version).to eq "NA"
+    end
+
+    it "should parse scopes" do
+      expect(subject.scopes.size).to eq 1
+      expect(subject.scopes.first).to eq "FB=1_3_4_5_8_13_14_15_18_19_31_32_35_37_38_39_40;IntervalDuration=900_3600;BlockDuration=Daily;HistoryLength=63072000;SubscriptionFrequency=Daily;"
+    end
+
+    it "should parse grant types" do
+      expect(subject.grant_types.size).to eq 3
+      expect(subject.grant_types.first).to eq "authorization_code"
+    end
+
+    it "should parse response types" do
+      expect(subject.response_types.size).to eq 1
+      expect(subject.response_types.first).to eq "code"
+    end
+
+    it "should parse registration_client_uri" do
+      expect(subject.registration_client_uri).to eq "https://api.pge.com/GreenButtonConnect/espi/1_1/resource/ApplicationInformation/00000000-2222-4444-6666-8888aaaacccc"
+    end
+
+    it "should parse registration_access_token" do
+      expect(subject.registration_access_token).to eq "00001111-2222-3333-4444-cccceeeeffff"
+    end
+
+    describe "#application_status" do
+      it "should return application status as a symbol" do
+        expect(subject.application_status).to eq :production
+      end
+    end
+
+    describe "#application_type" do
+      it "should return application type as a symbol" do
+        expect(subject.application_type).to eq :web
+      end
+    end
+
+    describe "#application_use" do
+      it "should return application use as a symbol" do
+        expect(subject.application_use).to eq nil
+      end
+    end
+  end
 end
