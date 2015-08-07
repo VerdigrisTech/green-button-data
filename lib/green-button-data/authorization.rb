ecb15c7f74c63d1345a130418d7fb9108cba6eb2
Andrew Jo <andrewjo@gmail.com>
2015-08-06 18:48:10 -0700

    Add attr_accessors and fix constructor

--- lib/green-button-data/authorization.rb
+++ lib/green-button-data/authorization.rb
@@ -2,6 +2,13 @@ module GreenButtonData
   class Authorization
     include Fetchable
 
+    attr_accessor :authorized_period,
+                  :published_period,
+                  :expires_at,
+                  :status,
+                  :resource_uri,
+                  :authorization_uri
+
     def initialize(attributes)
       if attributes.is_a?(Hash)
         @authorized_period = attributes[:authorized_period]
@@ -10,7 +17,7 @@ module GreenButtonData
         @status = attributes[:status]
         @resource_uri = attributes[:resource_uri]
         @authorization_uri = attributes[:authorization_uri]
-      elsif attributes.is_a?(GreenButtonData::Parser:Authorization)
+      elsif attributes.is_a?(GreenButtonData::Parser::Authorization)
         @authorized_period = attributes.authorized_period
         @published_period = attributes.published_period
         @expires_at = attributes.expires_at
@@ -18,18 +25,6 @@ module GreenButtonData
         @resource_uri = attributes.resource_uri
         @authorization_uri = attributes.authorization_uri
       end
-    end
-
-    def self.get(url = nil, options = nil)
-      @url = url or raise ArgumentError "url is require to fetch data"
-      @token = options[:token]
-      @client_ssl = options[:client_ssl]
-
-      @connection_options = {}
-      @connection_options.ssl = @client_ssl if @client_ssl
-
-      conn = Faraday.new @connection_options
-      conn.token_auth(@token) if @token
     end
   end
 end
