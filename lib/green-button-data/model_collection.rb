119f673a0fc3a4bc8be7cee0f0f4b0213bef16c1
Andrew Jo <andrewjo@gmail.com>
2015-08-06 18:50:15 -0700

    Add instance methods

--- lib/green-button-data/model_collection.rb
+++ lib/green-button-data/model_collection.rb
@@ -1,5 +1,25 @@
 module GreenButtonData
   class ModelCollection
     include Enumerable
+
+    def initialize
+      @models = []
+    end
+
+    def <<(model)
+      @models << model
+    end
+
+    def each
+      return enum_for(:each) unless block_given?
+
+      @models.each do |model|
+        yield model
+      end
+    end
+
+    def last
+      @models.last
+    end
   end
 end
