af1189ab4dc94dd42015d005c25a593dde63d496
Andrew Jo <andrewjo@gmail.com>
2015-08-10 13:25:00 -0700

    Add find method and add RDoc style comments

--- lib/green-button-data/fetchable.rb
+++ lib/green-button-data/fetchable.rb
@@ -1,3 +1,5 @@
+require 'uri'
+
 module GreenButtonData
   module Fetchable
     def self.included(base)
@@ -8,6 +10,14 @@ module GreenButtonData
       include Relations
       include Utilities
 
+      ##
+      # Returns all entries
+      #
+      # ==== Arguments
+      #
+      # * +url+ (OPTIONAL) - URL to fetch from. If not supplied, it defaults to
+      #                      the URLs in configuration.
+      # * +options+ - Options hash
       def all(url = nil, options = nil)
         if url.is_a?(Hash)
           # Assume it is an options Hash
@@ -21,8 +31,48 @@ module GreenButtonData
         return records
       end
 
+      ##
+      # Returns the first item in the ModelCollection from all entries returned
+      # by the API endpoint
+      #
+      # ==== Arguments
+      #
+      # * +url+ (OPTIONAL) - URL to fetch from. If not supplied, it defaults to
+      #                      the URLs in configuration.
+      # * +options+ - Options hash
       def first(url = nil, options = nil)
         return all(url, options).first
+      end
+
+      ##
+      # Finds an entry that matches the {id} in the URL of the form:
+      # https://services.greenbuttondata.org/DataCustodian/espi/1_1/resource/{model_name}/{id}
+      #
+      # ==== Arguments
+      #
+      # * +id+ - ID of the entry content. URL matching a single entry content
+      #          can be supplied in place of ID.
+      # * +url+ (OPTIONAL) - If specified, this URL is used to fetch data in
+      #                      place of global configuration.
+      # * +options+ - Options hash
+      #
+      # ==== Examples
+      #
+      # The following fetches data identically.
+      #
+      # ReadingType.find(1, token: "12345678-1024-2048-abcdef001234") # use global config
+      # ReadingType.find("https://services.greenbuttondata.org/DataCustodian/espi/1_1/resource/ReadingType/1", token: "12345678-1024-2048-abcdef001234") # override global config and use specific url
+      def find(id = nil, options = nil)
+        # If +id+ argument is a URL, set the url
+        url = if id =~ /\A#{URI::regexp}\z/
+          id
+        else
+          path_prefix = GreenButtonData.configuration
+                                       .send("#{class_name.underscore}_url")
+          "#{path_prefix}/#{id}"
+        end
+
+        return first(url, options)
       end
 
       def last(url = nil, options = nil)
@@ -49,7 +99,7 @@ module GreenButtonData
       end
 
       def records
-        @records ||= populate_model(feed)
+        @records ||= populate_models(feed)
       end
 
       def options
@@ -98,7 +148,7 @@ module GreenButtonData
         end
       end
 
-      def populate_model(feed)
+      def populate_models(feed)
         models = GreenButtonData::ModelCollection.new
 
         each_entry_content feed do |id, content, entry|
