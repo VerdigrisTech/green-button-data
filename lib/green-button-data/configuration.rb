8c9eff40125517c646c7459f246dfcaec009711e
Andrew Jo <andrewjo@gmail.com>
2015-08-09 20:35:33 -0700

    Add global URL configurations

--- lib/green-button-data/configuration.rb
+++ lib/green-button-data/configuration.rb
@@ -1,4 +1,12 @@
 module GreenButtonData
   class Configuration
+    attr_accessor :base_url,
+                  :application_information_path,
+                  :authorization_path,
+                  :interval_block_path,
+                  :meter_reading_path,
+                  :reading_type_path,
+                  :usage_point_path,
+                  :usage_summary_path
   end
 end
