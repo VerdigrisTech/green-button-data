81ef2ca4da22d9f32fed97a8318636baa55e2eff
Andrew Jo <andrewjo@gmail.com>
2015-08-09 19:01:50 -0700

    Add relations mixin

---
+++ lib/green-button-data/relations.rb
@@ -0,0 +1,24 @@
+module GreenButtonData
+  module Relations
+
+    ##
+    # Extracts related URLs from an Entry
+    #
+    # ==== Arguments
+    #
+    # * +entry+ - An instance of GreenButtonData::Parser::Entry
+    def construct_related_urls(entry)
+      related_urls = {}
+
+      entry.related.each do |related_url|
+        match_data = /\/(\w+)(\/(\d+))*$/.match(related_url)
+
+        unless match_data.nil?
+          related_urls[:"#{match_data[1].underscore}_url"] = related_url
+        end
+      end
+
+      related_urls
+    end
+  end
+end
