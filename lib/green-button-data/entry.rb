248076802280841da639819a0b54d83814676047
Andrew Jo <andrewjo@gmail.com>
2015-08-20 11:22:02 -0700

    Add comments

--- lib/green-button-data/entry.rb
+++ lib/green-button-data/entry.rb
@@ -6,6 +6,8 @@ module GreenButtonData
     attr_accessor :token
 
     def initialize(attributes)
+      # Automagically sets instance variables from attribute hash parsed from
+      # the GreenButtonData::Parser classes
       attributes.each do |key, value|
         self.instance_variable_set :"@#{key}", value
         singleton_class.class_eval do
@@ -13,6 +15,7 @@ module GreenButtonData
         end
       end
 
+      # Handle relations via related_urls
       @related_urls.is_a?(Hash) and @related_urls.each do |key, value|
         self.instance_variable_set :"@#{key}", value
         singleton_class.class_eval do
