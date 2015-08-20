59921f29ea8c5ae678b5c18f6c3e32e827bd5a0f
Andrew Jo <andrewjo@gmail.com>
2015-08-20 15:13:58 -0700

    Add PG&E namespaced elements

--- lib/green-button-data/parser/summary_measurement.rb
+++ lib/green-button-data/parser/summary_measurement.rb
@@ -26,6 +26,14 @@ module GreenButtonData
       element :'espi:uom', class: Integer, as: :uom
       element :'espi:value', class: Integer, as: :value
       element :'espi:readingTypeRef', as: :reading_type_ref
+
+      # Special case for PG&E generic namespacing
+      element :'ns0:powerOfTenMultiplier', class: Integer,
+              as: :power_of_ten_multiplier
+      element :'ns0:timeStamp', as: :time_stamp
+      element :'ns0:uom', class: Integer, as: :uom
+      element :'ns0:value', class: Integer, as: :value
+      element :'ns0:readingTypeRef', as: :reading_type_ref
     end
   end
 end
