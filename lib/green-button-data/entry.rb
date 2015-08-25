c3afbb6c2e0b189fd0f18ab84b995ff13c9d274c
Andrew Jo <andrewjo@gmail.com>
2015-08-25 12:09:12 -0700

    Dynamically build related resource accessor methods

--- lib/green-button-data/entry.rb
+++ lib/green-button-data/entry.rb
@@ -17,9 +17,53 @@ module GreenButtonData
 
       # Handle relations via related_urls
       @related_urls.is_a?(Hash) and @related_urls.each do |key, value|
-        self.instance_variable_set :"@#{key}", value
+        self.instance_variable_set :"@#{key}_url", value
         singleton_class.class_eval do
-          attr_accessor key.to_sym
+          attr_accessor "#{key}_url".to_sym
+        end
+
+        # Define accessor methods from pluralized resource names
+        self.class.send :define_method, "#{key.to_s.pluralize}" do |*args|
+          id = args[0]
+          options = args[1]
+
+          klazz_name = "GreenButtonData::#{key.to_s.camelize}"
+
+          # Handle deprecations
+          klazz_name.gsub! /ElectricPowerUsageSummary/, 'UsageSummary'
+
+          klazz = klazz_name.split('::')
+                            .inject(Object) { |obj, cls| obj.const_get cls }
+
+          collection = self.instance_variable_get "@#{key.to_s.pluralize}"
+          url = self.instance_variable_get "@#{key}_url"
+
+          # Make the ID argument optional
+          options ||= if id.is_a?(Hash)
+            id
+          else
+            {}
+          end
+
+          if id.is_a?(Numeric) || id.is_a?(String) || id.is_a?(Symbol)
+            # Try returning cached results first
+            collection and instance = collection.find_by_id(id)
+            cache_miss = instance.nil?
+
+            # On cache miss, send API request
+            instance ||= klazz.find "#{url}/#{id}", options
+
+            # Cache the result
+            collection ||= ModelCollection.new
+            collection << instance if cache_miss
+            self.instance_variable_set :"@#{key.to_s.pluralize}", collection
+
+            return instance
+          else
+            collection ||= klazz.all url, options
+
+            return collection
+          end
         end
       end
     end
--- lib/green-button-data/usage_point.rb
+++ lib/green-button-data/usage_point.rb
@@ -7,62 +7,22 @@ module GreenButtonData
 
     def initialize(attributes)
       super
-      @meter_readings = attributes[:meter_readings]
-      @usage_summary = attributes[:usage_summary]
+
+      # If deprecated usage summary name is used, create an alias
+      if self.class.instance_methods.grep(/electric_power_usage/).size > 0
+        warn "DEPRECATED: ElectricPowerUsageSummary has been renamed to UsageSummary"
+        singleton_class.class_eval do
+          alias_method :usage_summaries, :electric_power_usage_summaries
+        end
+      end
     end
 
     def service_category
       SERVICE[@kind]
     end
 
-    def meter_readings(id = nil)
-      if id.nil?
-        @meter_readings ||= MeterReading.all @meter_reading_url
-
-        return @meter_readings
-      else
-        # Try returning cached results first
-        @meter_readings and meter_reading = @meter_readings.find_by_id(id)
-        cache_miss = meter_reading.nil?
-
-        # Cache-miss; send API request
-        meter_reading ||= MeterReading.find("#{@meter_reading_url}/#{id}")
-
-        # Cache the result
-        unless @meter_readings
-          @meter_readings = ModelCollection.new
-          @meter_readings << meter_reading if cache_miss
-        end
-
-        return meter_reading
-      end
-    end
-
     def usage_summary_url
       return @usage_summary_url || @electric_power_usage_summary_url
-    end
-
-    def usage_summaries(id = nil)
-      if id.nil?
-        @usage_summaries ||= UsageSummary.all(usage_summary_url)
-
-        return @usage_summaries
-      else
-        # Try returning cached results first
-        @usage_summaries and usage_summary = @usage_summaries.find_by_id(id)
-        cache_miss = usage_summary.nil?
-
-        # Cache-miss; send API request
-        usage_summary ||= UsageSummary.find("#{usage_summary_url}/#{id}")
-
-        # Cache the result
-        unless @usage_summaries
-          @usage_summaries = ModelCollection.new
-          @usage_summaries << usage_summary if cache_miss
-        end
-
-        return usage_summary
-      end
     end
   end
 end
