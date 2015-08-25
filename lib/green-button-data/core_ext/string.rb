55f9dc63b0b168265fdf1c4e9aad699b8d1514f2
Andrew Jo <andrewjo@gmail.com>
2015-08-25 11:58:22 -0700

    Add camelize and pluralize methods

--- lib/green-button-data/core_ext/string.rb
+++ lib/green-button-data/core_ext/string.rb
@@ -1,4 +1,9 @@
 class String
+  def camelize
+    self.gsub(/(?<=_|^)(\w)/) { $1.upcase }
+        .gsub(/(?:_)(\w)/,'\1')
+  end
+
   def underscore
     # Shamelessly copied from Rails' ActiveSupport gem
     self.gsub(/::/, '/')
@@ -6,5 +11,21 @@ class String
         .gsub(/([a-z\d])([A-Z])/, '\1_\2')
         .tr("-", "_")
         .downcase
+  end
+
+  def pluralize
+    result = self.dup
+
+    if self.empty?
+      return result
+    else
+      [
+        [/([^aeiouy]|qu)y$/i, '\1ies'],
+        [/s$/i, 's'],
+        [/$/, 's']
+      ].each { |(rule, replacement)| break if result.sub!(rule, replacement) }
+
+      return result
+    end
   end
 end
