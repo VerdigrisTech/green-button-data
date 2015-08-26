87508f9f8223f38245515f6538f5b07adb6c97b0
Andrew Jo <andrewjo@gmail.com>
2015-08-26 15:32:35 -0700

    Remove attr_accessor
    Removes attr_accessor to enable each classes that inherit from Entry class to
    have custom getter methods

--- lib/green-button-data/entry.rb
+++ lib/green-button-data/entry.rb
@@ -10,9 +10,6 @@ module GreenButtonData
       # the GreenButtonData::Parser classes
       attributes.each do |key, value|
         self.instance_variable_set :"@#{key}", value
-        singleton_class.class_eval do
-          attr_accessor key.to_sym
-        end
       end
 
       # Handle relations via related_urls
