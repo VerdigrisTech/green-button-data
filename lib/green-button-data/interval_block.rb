b2b46620f279eeed11f474bb463003ba7116ce60
Andrew Jo <andrewjo@gmail.com>
2015-08-27 14:18:13 -0700

    Add to_a method to convert to generic array

--- lib/green-button-data/interval_block.rb
+++ lib/green-button-data/interval_block.rb
@@ -1,5 +1,7 @@
 module GreenButtonData
   class IntervalBlock < Entry
+    include Utilities
+
     attr_accessor :length, :starts_at, :ends_at, :duration
 
     def initialize(attributes)
@@ -9,6 +11,23 @@ module GreenButtonData
       @ends_at = @interval.ends_at local: true
       @duration = @interval.duration
       @length = @interval_readings.size
+    end
+
+    ##
+    # Returns an array representation of all the interval data
+    def to_a
+      result = []
+
+      @interval_readings.each do |interval_reading|
+        result << {
+          starts_at: interval_reading.time_period.starts_at(local: true),
+          ends_at: interval_reading.time_period.ends_at(local: true),
+          duration: interval_reading.time_period.duration,
+          value: interval_reading.value
+        }
+      end
+
+      return result
     end
   end
 end
