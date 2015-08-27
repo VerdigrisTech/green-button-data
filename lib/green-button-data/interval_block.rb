4ea809dfb1b30ed740cebc589f458c080caece05
Andrew Jo <andrewjo@gmail.com>
2015-08-26 17:23:06 -0700

    Initial IntervalBlock implementation

--- lib/green-button-data/interval_block.rb
+++ lib/green-button-data/interval_block.rb
@@ -1,15 +1,14 @@
 module GreenButtonData
-  class IntervalBlock
-    include Fetchable
-
-    attr_reader :id
+  class IntervalBlock < Entry
+    attr_accessor :length, :starts_at, :ends_at, :duration
 
     def initialize(attributes)
-      @id = attributes[:id]
-    end
+      super
 
-    def interval_readings
-      
+      @starts_at = @interval.starts_at
+      @ends_at = @interval.ends_at
+      @duration = @interval.duration
+      @length = @interval_readings.size
     end
   end
 end
