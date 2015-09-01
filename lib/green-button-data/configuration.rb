edeec08ddd715049ac83bcb5f62b68bfdb374e5b
Andrew Jo <andrewjo@gmail.com>
2015-08-31 19:00:58 -0700

    Refactor configuration options
    - Add support for Subscription id
    - Fix URL paths that has optional Subscription id requirements
    - Remove setter url methods

--- lib/green-button-data/configuration.rb
+++ lib/green-button-data/configuration.rb
@@ -8,6 +8,7 @@ module GreenButtonData
                   :interval_block_path,
                   :meter_reading_path,
                   :reading_type_path,
+                  :subscription_path,
                   :usage_point_path,
                   :usage_summary_path
 
@@ -31,54 +32,94 @@ module GreenButtonData
       @authorization_path = uri.path
     end
 
-    def interval_block_url
-      return URI.join(@base_url, @interval_block_path).to_s
+    def interval_block_url(kwargs = {})
+      subscription_id = kwargs[:subscription_id]
+      usage_point_id = kwargs[:usage_point_id]
+      meter_reading_id = kwargs[:meter_reading_id]
+      interval_block_id = kwargs[:interval_block_id]
+
+      uri = if subscription_id && usage_point_id && meter_reading_id
+        meter_reading_uri = meter_reading_url(
+          subscription_id: subscription_id,
+          usage_point_id: usage_point_id,
+          meter_reading_id: meter_reading_id
+        )
+
+        URI.join meter_reading_uri, @interval_block_path
+      else
+        URI.join @base_url, @interval_block_path
+      end
+
+      uri = URI.join uri, "#{interval_block_id}/" if interval_block_id
+
+      return uri.to_s
     end
 
-    def interval_block_url=(url)
-      uri = URI.parse url
-      @base_url = "#{uri.scheme}://#{uri.host}"
-      @interval_block_path = uri.path
-    end
+    def meter_reading_url(kwargs = {})
+      subscription_id = kwargs[:subscription_id]
+      usage_point_id = kwargs[:usage_point_id]
+      meter_reading_id = kwargs[:meter_reading_id]
 
-    def meter_reading_url
-      return URI.join(@base_url, @meter_reading_path).to_s
-    end
+      uri = if subscription_id && usage_point_id
+        usage_point_uri = usage_point_url(
+          subscription_id: subscription_id,
+          usage_point_id: usage_point_id
+        )
 
-    def meter_reading_url=(url)
-      uri = URI.parse url
-      @base_url = "#{uri.scheme}://#{uri.host}"
-      @meter_reading_path = uri.path
+        URI.join usage_point_uri, @meter_reading_path
+      else
+        URI.join @base_url, @meter_reading_path
+      end
+
+      uri = URI.join uri, "#{meter_reading_id}/" if meter_reading_id
+
+      return uri.to_s
     end
 
     def reading_type_url
       return URI.join(@base_url, @reading_type_path).to_s
     end
 
-    def reading_type_url=(url)
-      uri = URI.parse url
-      @base_url = "#{uri.scheme}://#{uri.host}"
-      @reading_type_path = uri.path
+    def subscription_url(id)
+      uri = URI.join @base_url, @subscription_path
+      uri = URI.join uri, "#{id}/" if id
+      return uri.to_s
     end
 
-    def usage_point_url
-      return URI.join(@base_url, @usage_point_path).to_s
+    def usage_point_url(kwargs = {})
+      subscription_id = kwargs[:subscription_id]
+      usage_point_id = kwargs[:usage_point_id]
+
+      uri = if subscription_id
+        subscription_uri = subscription_url subscription_id
+
+        URI.join subscription_uri, @usage_point_path
+      else
+        URI.join @base_url, @usage_point_path
+      end
+
+      uri = URI.join uri, "#{usage_point_id}/" if usage_point_id
+
+      return uri.to_s
     end
 
-    def usage_point_url=(url)
-      uri = URI.parse url
-      @base_url = "#{uri.scheme}://#{uri.host}"
-      @usage_point_path = uri.path
-    end
+    def usage_summary_url(kwargs = {})
+      subscription_id = kwargs[:subscription_id]
+      usage_point_id = kwargs[:usage_point_id]
 
-    def usage_summary_url
-      return URI.join(@base_url, @usage_summary_path).to_s
-    end
+      if subscription_id && usage_point_id
+        usage_point_uri = usage_point_url subscription_id: subscription_id,
+                                          usage_point_id: usage_point_id
 
-    def usage_summary_url=(url)
-      uri = URI.parse url
-      @base_url = "#{uri.scheme}://#{uri.host}"
-      @usage_summary_path = uri.path
+        return URI.join(usage_point_uri, @usage_summary_path).to_s
+      elsif subscription_id
+        raise ArgumentError.new "Missing required argument: usage_point_id"
+      elsif usage_point_id
+        raise ArgumentError.new "Missing required argument: subscription_id"
+      else
+        raise ArgumentError.new "Missing required arguments: subscription_id," +
+                                " usage_point_id"
+      end
     end
   end
 end
